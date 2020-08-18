345f86df72290077d09ea78a3f90d2fe0d20b08e
Tim Band <t.band>
2020-08-18 21:09:07 +0100

    Tests run with Selenium, not Zombie

--- test/index.js
+++ test/index.js
@@ -2,17 +2,18 @@
 
 const { describe, before, after, it } = require('mocha');
 const assert = require('assert');
-const Browser = require('zombie');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const http = require('http');
 const fs = require('fs');
 const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
+const { exit } = require('process');
 
 describe('dataentrygrid', function () {
-  const browser = new Browser();
   let server = null;
+  let driver = null;
 
   before(async function () {
-    server = http.createServer(function(req, resp) {
+    server = await http.createServer(function(req, resp) {
       fs.readFile("./test" + req.url, function(error, content) {
         let code = 200;
         if (error) {
@@ -27,17 +28,30 @@ describe('dataentrygrid', function () {
         resp.writeHead(code, {"Content-Type": "text/html"});
         resp.end(content, "utf-8");
       });
-    }).listen(3004);
-    await browser.visit("http://localhost:3004/dataentrygrid.html");
+    });
+    server.listen(3004);
+    driver = new Builder().forBrowser('firefox').build();
+  });
+
+  after(function(done) {
+    this.timeout(9000);
+    server.close(function() {
+      driver.quit().then(function() { done() });
+    });
   });
 
   describe('highlight (visual and API)', function () {
+    this.timeout(5000);
+
+    before(async function() {
+      await driver.get("http://localhost:3004/dataentrygrid.html");
+    })
 
     it('can be set with a mouse click', async function() {
       const row = 0;
       const column = 1;
-      await clickCell(browser, row, column);
-      await checkSelection(browser, row, row, column, column);
+      await clickCell(driver, row, column);
+      await checkSelection(driver, row, row, column, column);
     });
 
     it('can be set with a mouse drag', async function() {
@@ -45,48 +59,46 @@ describe('dataentrygrid', function () {
       const startColumn = 1;
       const endRow = 0;
       const endColumn = 2;
-      await mouseDragCells(browser,
+      await mouseDragCells(driver,
         [[startRow,startColumn], [0,1], [endRow,endColumn]]);
-      await checkSelection(browser, startRow, endRow, startColumn, endColumn);
+      await checkSelection(driver, startRow, endRow, startColumn, endColumn);
     });
 
     it('moves with the cursor keys', async function() {
-      await clickCell(browser, 1, 1);
-      const table = await browser.querySelector('#input');
-      await sendKeys(browser, table, ['ArrowUp']);
-      await checkSelection(browser, 0, 0, 1, 1);
-      await sendKeys(browser, table, ['ArrowLeft', 'ArrowLeft']);
-      await checkSelection(browser, 0, 0, 0, 0);
-      await sendKeys(browser, table, ['ArrowDown']);
-      await checkSelection(browser, 1, 1, 0, 0);
-      await sendKeys(browser, table, ['ArrowRight', 'ArrowRight', 'ArrowRight', 'ArrowRight', 'ArrowRight']);
-      await checkSelection(browser, 1, 1, 1, 1);
+      await clickCell(driver, 1, 1);
+      const table = await getTable(driver);
+      await table.sendKeys(Key.ARROW_UP);
+      await checkSelection(driver, 0, 0, 1, 1);
+      await table.sendKeys(Key.ARROW_LEFT, Key.ARROW_LEFT);
+      await checkSelection(driver, 0, 0, 0, 0);
+      await table.sendKeys(Key.ARROW_DOWN);
+      await checkSelection(driver, 1, 1, 0, 0);
+      await table.sendKeys(Key.ARROW_RIGHT, Key.ARROW_RIGHT);
+      await checkSelection(driver, 1, 1, 1, 1);
     });
 
     it('does not move off the ends', async function() {
-      await clickCell(browser, 1, 1);
-      const table = await browser.querySelector('#input');
-      await sendKeys(browser, table, ['ArrowUp', 'ArrowUp', 'ArrowUp']);
-      await checkSelection(browser, 0, 0, 1, 1);
-      await sendKeys(browser, table, ['ArrowLeft', 'ArrowLeft', 'ArrowLeft']);
-      await checkSelection(browser, 0, 0, 0, 0);
-      const rc = await getRowCount(browser);
-      const downs = Array(2 + 2 * rc).fill('ArrowDown');
-      await sendKeys(browser, table, downs);
-      await checkSelection(browser, rc-1, rc-1, 0, 0);
-      const cc = await getColumnCount(browser);
-      const rights = Array(2 + 2 * cc).fill('ArrowRight');
-      await sendKeys(browser, table, rights);
-      await checkSelection(browser, rc-1, rc-1, cc-1, cc-1);
+      await clickCell(driver, 1, 1);
+      const table = await getTable(driver);
+      await repeatKey(table, 3, Key.ARROW_UP);
+      await checkSelection(driver, 0, 0, 1, 1);
+      await repeatKey(table, 5, Key.ARROW_LEFT);
+      await checkSelection(driver, 0, 0, 0, 0);
+      const rc = await getRowCount(driver);
+      await repeatKey(table, 2 + rc, Key.ARROW_DOWN);
+      await checkSelection(driver, rc-1, rc-1, 0, 0);
+      const cc = await getColumnCount(driver);
+      await repeatKey(table, 2 + 2 * cc, Key.ARROW_RIGHT);
+      await checkSelection(driver, rc-1, rc-1, cc-1, cc-1);
     });
 
-    it('gets extended with shift-arrows', async function() {
+/*    it('gets extended with shift-arrows', async function() {
     });
     it('does not extend past the ends', async function() {
-    });
+    });*/
   });
 
-  describe('cell content text', function() {
+/*8  describe('cell content text', function() {
     it('is set with typing', async function() {
       // finish typing with return, highlight moves down
       // finish typing with tab, highlight moves right
@@ -133,28 +145,23 @@ describe('dataentrygrid', function () {
     it('', async function() {
     });
   });*/
-
-  after(function() {
-    server.close();
-  });
 });
 
-async function sendKeys(browser, table, keys) {
-  for (const i in keys) {
-    const keydown = await new browser.window.KeyboardEvent('keydown', {
-      key: keys[i]
-    });
-    const keypress = await new browser.window.KeyboardEvent('keypress', {
-      key: keys[i]
-    });
-    await table.dispatchEvent(keydown);
-    await table.dispatchEvent(keypress);
-    }
+async function repeatKey(element, times, key) {
+  await element.sendKeys.apply(element, repeat(times, key));
 }
 
-async function checkSelection(browser, startRow, endRow, startColumn, endColumn) {
+function repeat(times, x) {
+  return Array(times).fill(x);
+}
+
+function getTable(driver) {
+  return driver.findElement(By.id('input'));
+}
+
+async function checkSelection(driver, startRow, endRow, startColumn, endColumn) {
   // API selection
-  const sel = await getSelection(browser);
+  const sel = await getSelection(driver);
   assert.equal(sel.anchorRow, startRow);
   assert.equal(sel.anchorColumn, startColumn);
   assert.equal(sel.selectionRow, endRow);
@@ -164,36 +171,38 @@ async function checkSelection(browser, startRow, endRow, startColumn, endColumn)
   const maxR = Math.max(startRow, endRow);
   const minC = Math.min(startColumn, endColumn);
   const maxC = Math.max(startColumn, endColumn);
-  const rowCount = await getRowCount(browser);
-  const columnCount = await getColumnCount(browser);
+  const rowCount = await getRowCount(driver);
+  const columnCount = await getColumnCount(driver);
   for (let r = 0; r !== rowCount; ++r) {
     for (let c = 0; c !== columnCount; ++c) {
+      const cell = await getCell(driver, r, c);
       if (minR <= r && r <= maxR && minC <= c && c <= maxC) {
-        browser.assert.hasClass(cellSelector(r, c), 'selected',
+        assertHasClass(cell, 'selected',
           '('+r+','+c+') should have been selected');
       }
       else {
-        browser.assert.hasNoClass(cellSelector(r, c), 'selected',
+        assertHasNoClass(cell, 'selected',
           '('+r+','+c+') should not have been selected');
       }
       if (r === startRow && c === startColumn) {
-        browser.assert.hasClass(cellSelector(r, c), 'anchor',
+        assertHasClass(cell, 'anchor',
           '('+r+','+c+') should have been the anchor');
       }
       else {
-        browser.assert.hasNoClass(cellSelector(r, c), 'anchor',
+        assertHasNoClass(cell, 'anchor',
           '('+r+','+c+') should not have been the anchor');
       }
     }
   }
 }
 
-async function clickCell(browser, row, column) {
-  await browser.click(cellSelector(row, column));
+async function clickCell(driver, row, column) {
+  const cell = await getCell(driver, row, column);
+  await cell.click();
 }
 
-async function getSelection(browser) {
-  const sel = await browser.evaluate('window.dataEntryGrid.getSelection();');
+async function getSelection(driver) {
+  const sel = await driver.executeScript('return window.dataEntryGrid.getSelection();');
   // columns are zero-based but rows are one-based;
   // this should be corrected.
   sel.anchorRow -= 1;
@@ -201,41 +210,61 @@ async function getSelection(browser) {
   return sel;
 }
 
-async function getRowCount(browser) {
+async function getRowCount(driver) {
   // columns are zero-based but rows are one-based;
   // this should be corrected.
-  return await browser.evaluate('window.dataEntryGrid.rowCount();') - 1;
+  return await driver.executeScript('return window.dataEntryGrid.rowCount();') - 1;
 }
 
-async function getColumnCount(browser) {
-  return await await browser.evaluate('window.dataEntryGrid.columnCount();');
+async function getColumnCount(driver) {
+  return await driver.executeScript('return window.dataEntryGrid.columnCount();');
 }
 
-async function mouseDragCells(browser, coords) {
-  const mouseDownEvent = new browser.window.MouseEvent('mousedown', {
-    button: 0, buttons: 1, bubbles: true, cancelable: true, detail: 0});
-  const mouseUpEvent = new browser.window.MouseEvent('mouseup', {
-    button: 0, buttons: 1, bubbles: true, cancelable: true, detail: 0});
-  const mouseEnterEvent = new browser.window.MouseEvent('mouseenter', {
-    buttons: 1, bubbles: true, cancelable: true, detail: 0});
-  const mouseLeaveEvent = new browser.window.MouseEvent('mouseleave', {
-    buttons: 1, bubbles: true, cancelable: true, detail: 0});
+async function mouseDragCells(driver, coords) {
   let [r,c] = coords[0];
   let sel = cellSelector(r,c);
-  let element = browser.querySelector(sel);
-  await browser.fire(sel, 'mouseenter');
-  await element.dispatchEvent(mouseDownEvent);
-  await browser.fire(sel, 'click');
+  const element = getCell(driver, r, c);
+  let actions = driver.actions().move({origin:element}).press();
   for (let i = 1; i != coords.length; ++i) {
     [r,c] = coords[i];
-    await element.dispatchEvent(mouseLeaveEvent);
-    sel = cellSelector(r,c);
-    element = browser.querySelector(sel);
-    await element.dispatchEvent(mouseEnterEvent);
+    const el = getCell(driver, r, c);
+    actions = actions.move({origin: el});
   }
-  await element.dispatchEvent(mouseUpEvent);
+  await actions.release().perform();
+}
+
+async function getCell(driver, row, column) {
+  return driver.findElement(cellSelector(row, column));
 }
 
 function cellSelector(row, column) {
-  return `#input tbody tr:nth-child(${row + 1}) td:nth-child(${column + 2})`;
+  return By.css(`#input tbody tr:nth-child(${row + 1}) td:nth-child(${column + 2})`);
+}
+
+async function assertHasClass(element, c, message) {
+  const cs = await element.getAttribute('class');
+  if (cs && (' '+cs+' ').includes(c)) {
+    return;
+  }
+  if (!message) {
+    const tag = await element.getTagName();
+    const id = await element.getId();
+    const name = id? `${tag} element ${id}` : `${tag} element`;
+    message = `${name} does not have the class ${c} (rather, it has ${cs})`;
+  }
+  assert.fail(message);
+}
+
+async function assertHasNoClass(element, c, message) {
+  const cs = await element.getAttribute('class');
+  if (!cs || !(' '+cs+' ').includes(c)) {
+    return;
+  }
+  if (!message) {
+    const tag = await element.getTagName();
+    const id = await element.getId();
+    const name = id? `${tag} element ${id}` : `${tag} element`;
+    message = `${name} should not have the class ${c} (it has ${cs})`;
+  }
+  assert.fail(message);
 }
